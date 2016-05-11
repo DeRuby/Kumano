164dfe593d6f9c00405dd516a3e3aa7ac0939481
fktan5 <email@fktan5.net>
2016-05-11 10:22:09 +0900

    spec init

---
+++ spec/kumanoSpec.js
@@ -0,0 +1,5 @@
+// Hubot classes
+//var Robot = require("hubot/src/robot");
+var TextMessage = require("hubot/src/message").TextMessage;
+// Load assertion methods to this scope
+var robot = new Robot(null, 'mock-adapter', yes, 'KumanoTest');
